ef8b5635611afeb10c938721777442ac3d55c484
robert corrie <robert.corrie@gmail.com>
2015-12-01 17:10:54 +0900

    The original commit for Assignment1 file

---
+++ Foothill.java
@@ -0,0 +1,50 @@
+/*
+ * CS 1A Assignment 1 - Hello World! (Foothill College).
+ * A first program to demonstrate the println() method call.
+ * Written by Robert Kyle Corrie, 09/30/2015. 
+ */
+public class Foothill
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Hello world!");
+      System.out.println("My name is Robert Kyle Corrie");
+      System.out.println("My student ID is 10743025\n");
+      System.out.println("Syllabus detail #1: Participation in the public"
+         + " discussions is not required but\n is recommended.\n");
+      System.out.println("Syllabus detail #2: Students will be dropped from"
+         + " the course if they do not log\n in for 10 consecutive days.\n");
+      System.out.println("Syllabus detail #3: Collaborating or copying"
+         + " homework assignments is not\n allowed.\n");
+      System.out.println("Specific lab detail #1: No points will be given for"
+         + " a non-working program; it is\n always better to take a late day"
+         + " for a small deduction and get the program\n working.\n");
+      System.out.println("Specific lab detail #2: Variables should be given"
+         + " descriptive names unless the\n variable is a loop counter when"
+         + " single letters may be used.");
+   }
+}
+
+/* ------------------ paste of run from console window -------------------
+
+Hello world!
+My name is Robert Kyle Corrie
+My student ID is 10743025
+
+Syllabus detail #1: Participation in the public discussions is not required but
+ is recommended.
+
+Syllabus detail #2: Students will be dropped from the course if they do not log
+ in for 10 consecutive days.
+
+Syllabus detail #3: Collaborating or copying homework assignments is not
+ allowed.
+
+Specific lab detail #1: No points will be given for a non-working program; it is
+ always better to take a late day for a small deduction and get the program
+ working.
+
+Specific lab detail #2: Variables should be given descriptive names unless the
+ variable is a loop counter when single letters may be used.
+
+-------------------------------------------------------------------------- */
